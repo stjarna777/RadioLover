802b20d5114bfceba749eadf0cc23e7d70cc1a83
anna_stjarna <artemyeva@gmail.com>
2023-02-24 10:34:01 +0300

    File for Radio class added

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,81 @@
+package ru.netology;
+
+public class Radio {
+    private int currentRadioStation;
+    private int currentVolume;
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
+
+        if (newCurrentRadioStation < 0) {
+            return;
+        }
+        if (newCurrentRadioStation > 9) {
+            return;
+        }
+        currentRadioStation = newCurrentRadioStation;
+    }
+
+    public void setToMaxRadioStation() {
+        currentRadioStation = 9;
+    }
+
+    public void nextRadioStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation = currentRadioStation + 1;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void prevRadioStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation = currentRadioStation - 1;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void setToMinRadioStation() {
+        currentRadioStation = 0;
+    }
+
+    // Громкость
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void setToMaxVolume() {
+        currentVolume = 10;
+    }
+
+    public void increaseToNextVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void setToMinVolume() {
+        currentVolume = 0;
+    }
+
+    public void increaseToPrevVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
